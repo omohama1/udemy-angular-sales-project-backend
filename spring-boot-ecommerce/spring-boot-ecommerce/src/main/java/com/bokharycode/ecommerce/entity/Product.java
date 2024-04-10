ea20f4c7545bf513a5850637cf5f94f7b4e397b1
Omar Bin Mohamad <omarbmohamad@gmail.com>
2024-04-10 13:19:53 -0500

    Adding Product and ProductCategory entities

---
+++ spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/bokharycode/ecommerce/entity/Product.java
@@ -0,0 +1,46 @@
+package com.bokharycode.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "sku")
+    private String sku;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+    @Column(name = "image_url")
+    private String imageUrl;
+    @Column(name = "active")
+    private boolean active;
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+}
