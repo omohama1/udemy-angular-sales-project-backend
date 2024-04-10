697b4072f2dab7b3099e3f116fe52e3eb6198748
Omar Bin Mohamad <omarbmohamad@gmail.com>
2024-04-10 14:43:01 -0500

    product update

--- spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/bokharycode/ecommerce/entity/Product.java
+++ spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/bokharycode/ecommerce/entity/Product.java
@@ -1,5 +1,7 @@
 package com.bokharycode.ecommerce.entity;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -22,23 +24,36 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category")
+    private ProductCategory category;
+
     @Column(name = "sku")
     private String sku;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "description")
     private String description;
+
     @Column(name = "unit_price")
     private BigDecimal unitPrice;
+
     @Column(name = "image_url")
     private String imageUrl;
+
     @Column(name = "active")
     private boolean active;
+
     @Column(name = "units_in_stock")
     private int unitsInStock;
+
     @Column(name = "date_created")
     @CreationTimestamp
     private Date dateCreated;
+
     @Column(name = "last_updated")
     @UpdateTimestamp
     private Date lastUpdated;
