edbb72b81fc5eecada54cf194b1438773b5acbbd
Omar Qasirov <omarbmohamad@gmail.com>
2024-04-12 11:50:54 -0500

    Adding cors annotation to productcategory repo

--- spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/bokharycode/ecommerce/dao/ProductCategoryRepository.java
+++ spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/bokharycode/ecommerce/dao/ProductCategoryRepository.java
@@ -3,8 +3,10 @@ package com.bokharycode.ecommerce.dao;
 import com.bokharycode.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin("http://localhost:4200") // Frontend seems to work without this. Can't find why
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category"s)
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 }
