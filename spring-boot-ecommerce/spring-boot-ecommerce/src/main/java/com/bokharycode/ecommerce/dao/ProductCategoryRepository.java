9488ae19c25608e12e66c931915b7ef73d5f48d7
Omar Qasirov <omarbmohamad@gmail.com>
2024-04-12 12:08:51 -0500

    Removing typo

--- spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/bokharycode/ecommerce/dao/ProductCategoryRepository.java
+++ spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/bokharycode/ecommerce/dao/ProductCategoryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200") // Frontend seems to work without this. Can't find why
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category"s)
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 }
