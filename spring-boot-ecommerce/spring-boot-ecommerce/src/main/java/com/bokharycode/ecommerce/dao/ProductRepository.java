2bf1c21c11da029a07213e5b3015582bbe6d301e
Omar Bin Mohamad <omarbmohamad@gmail.com>
2024-05-13 17:48:23 -0500

    Adding repo method

--- spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/bokharycode/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/bokharycode/ecommerce/dao/ProductRepository.java
@@ -2,11 +2,11 @@ package com.bokharycode.ecommerce.dao;
 
 import com.bokharycode.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.awt.print.Pageable;
 
 @CrossOrigin("http://localhost:4200") // Frontend seems to work without this. Can't find why
 public interface ProductRepository extends JpaRepository<Product, Long> {
